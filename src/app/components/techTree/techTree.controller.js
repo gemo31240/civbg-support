c258f220e474c5c5ae1a2b0b74c16d2302124645
wiro34 <too@wirog.net>
2015-06-20 13:49:37 +0000

    兵力自動更新

--- src/app/components/techTree/techTree.controller.js
+++ src/app/components/techTree/techTree.controller.js
@@ -38,7 +38,7 @@ class TechTreeController {
 
   isNewtonUsed() {
     var containsNewton = (techIds) => _.contains(techIds, 'newton');
-    var newtonScanner = (player) => containsNewton(player.tech.one) || containsNewton(player.tech.two);
+    var newtonScanner = (player) => containsNewton(player.tech.one) || containsNewton(player.tech.two) || containsNewton(player.tech.three) || containsNewton(player.tech.four);
     return _.find(this.players, newtonScanner);
   }
 
@@ -67,6 +67,7 @@ class TechTreeController {
       this.player.tech[level] = {};
     }
     this.player.tech[level][length + 1] = techId;
+    this.updateArmsRank();
   }
 
   removeTech(level, techId) {
@@ -74,8 +75,45 @@ class TechTreeController {
       var index = this.player.tech[level].indexOf(techId);
       if (index >= 0) {
         this.player.tech[level].splice(index, 1);
+        this.updateArmsRank();
       }
     }
+  }
+
+  updateArmsRank() {
+    var techArray = _.compact(_.flatten([
+      _.values(this.player.tech.one),
+      _.values(this.player.tech.two),
+      _.values(this.player.tech.three),
+      _.values(this.player.tech.four)
+    ]));
+
+    this.player.arms = {
+      sword: 1,
+      cannon: 1,
+      cavalry: 1,
+      airforce: 0
+    };
+
+    _.chain(techArray).map(this.Tech.findById).filter((tech) => angular.isString(tech.arms)).each((tech) => {
+      if (tech.arms.indexOf('歩兵') === 0) {
+        this.player.arms.sword = _.max([this.player.arms.sword, +tech.arms.slice(-1)]);
+      }
+      if (tech.arms.indexOf('砲兵') === 0) {
+        this.player.arms.cannon = _.max([this.player.arms.cannon, +tech.arms.slice(-1)]);
+      }
+      if (tech.arms.indexOf('騎兵') === 0) {
+        this.player.arms.cavalry = _.max([this.player.arms.cavalry, +tech.arms.slice(-1)]);
+      }
+      if (tech.arms.indexOf('全兵') === 0) {
+        this.player.arms.sword = _.max([this.player.arms.sword, +tech.arms.slice(-1)]);
+        this.player.arms.cannon = _.max([this.player.arms.cannon, +tech.arms.slice(-1)]);
+        this.player.arms.cavalry = _.max([this.player.arms.cavalry, +tech.arms.slice(-1)]);
+      }
+      if (tech.arms.indexOf('飛行機') === 0) {
+        this.player.arms.airforce = 1;
+      }
+    });
   }
 }
 
